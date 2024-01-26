06bf5597f83dcf06b0ef82948177cd2bcbeb901b
alexmshow <maerincah@gmail.com>
2024-01-27 02:27:09 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 pip install pyinstaller
 pip install -r requirements.txt
-pyinstaller hent.py --icon=icon.ico --onefile --noconsole
+pyinstaller hent.py --icon=icon.ico --onefile --noconsole -n HentaiMaster
 mkdir dist\resources
 copy resources dist\resources
